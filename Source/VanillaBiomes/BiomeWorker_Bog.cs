d12f7fa223b4c61db9c7bdb7e48e7381cfb91b20
zyllle <zyll3on@gmail.com>
2019-12-04 17:49:19 -0700

    No beaches for icebergs and sandbars

---
+++ Source/VanillaBiomes/BiomeWorker_Bog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+
+namespace VanillaBiomes
+{
+    public class BiomeWorker_Bog : BiomeWorker
+    {
+        public override float GetScore(Tile tile, int tileID)
+        {
+            return 100;
+
+
+        }
+    }
+}
--- Source/VanillaBiomes/HarmonyPatches.cs
+++ Source/VanillaBiomes/HarmonyPatches.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 using RimWorld;
 using Verse;
 using Harmony;
-
+using RimWorld.Planet;
 
 namespace VanillaBiomes
 {
@@ -14,7 +14,61 @@ namespace VanillaBiomes
     static class HarmonyPatches
     {
 
+        static HarmonyPatches()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("rimworld.antinium");
 
-       
+            // find the AddFoodPoisoningHediff method of the class RimWorld.FoodUtility
+            MethodInfo targetmethod = AccessTools.Method(typeof(RimWorld.Planet.World), "CoastDirectionAt");
+
+            // find the static method to call before (i.e. Prefix) the targetmethod
+            HarmonyMethod prefixmethod = new HarmonyMethod(typeof(VanillaBiomes.HarmonyPatches).GetMethod("CoastDirectionAt_Prefix"));
+
+            // patch the targetmethod, by calling prefixmethod before it runs, with no postfixmethod (i.e. null)
+            harmony.Patch(targetmethod, prefixmethod, null);
+
+        }
+
+
+        // from RF-Archipelagos
+        public static bool CoastDirectionAt_Prefix(int tileID, ref Rot4 __result, ref World __instance)
+        {
+            Log.Message("Starting beach fix");
+            var world = Traverse.Create(__instance);
+            WorldGrid worldGrid = world.Field("grid").GetValue<WorldGrid>();
+            if (worldGrid[tileID].biome.defName.Contains("NoBeach"))
+            {
+                Log.Message("Should be no beach");
+                __result = Rot4.Invalid;
+                return false;
+            }
+            Log.Message("Beach should exist");
+            return true;
+        }
+
+
+        //[HarmonyPatch(typeof(World), "CoastDirectionAt", null)]
+        //public static class World_CoastDirectionAt
+        //{
+        //    public static bool CoastDirectionAt_Prefix(int tileID, ref Rot4 __result, ref World __instance)
+        //    {
+        //        Log.Message("Starting beach fix");
+        //        var world = Traverse.Create(__instance);
+        //        WorldGrid worldGrid = world.Field("grid").GetValue<WorldGrid>();
+        //        if (worldGrid[tileID].biome.defName.Contains("NoBeach"))
+        //        {
+        //            Log.Message("Should be no beach");
+        //            __result = Rot4.Invalid;
+        //            return false;
+        //        }
+        //        Log.Message("Beach should exist");
+        //        return true;
+        //    }
+        //}
+
+
+
     }
+
+
 }
