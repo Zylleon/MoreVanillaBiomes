b36f6fbd9e57bf21e0051d5912b5f7c6b9fee0c4
zyllle <zyll3on@gmail.com>
2020-02-23 17:02:43 -0700

    sandbar fix

---
+++ 1.1/Source/VanillaBiomes/BiomeWorker_Sandbar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld.Planet;
+using RimWorld;
+using Verse;
+
+namespace VanillaBiomes
+{
+    public class BiomeWorker_Sandbar : BiomeWorker
+    {
+        public override float GetScore(Tile tile, int tileID)
+        {
+                return -100f;
+        }
+    }
+}
