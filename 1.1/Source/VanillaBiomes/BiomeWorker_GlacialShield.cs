a08601860f3acfe5028432aa41bf3dcab48fe867
Zylleon <56230821+Zylleon@users.noreply.github.com>
2022-04-03 12:59:11 -0600

    started work on glacial shield

---
+++ 1.1/Source/VanillaBiomes/BiomeWorker_GlacialShield.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld.Planet;
+using RimWorld;
+using Verse;
+using Verse.Noise;
+
+namespace VanillaBiomes
+{
+    public class BiomeWorker_GlacialShield : BiomeWorker
+    {
+        public override float GetScore(Tile tile, int tileID)
+        {
+			
+			if (tile.WaterCovered)
+			{
+				return -100f;
+			}
+			
+			return -100f;
+
+		}
+    }
+}
