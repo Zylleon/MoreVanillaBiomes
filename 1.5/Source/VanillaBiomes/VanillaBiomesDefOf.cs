a00cf5f1ad542f85b9e170ae16a74520a1ec8fcc
Zylleon <56230821+Zylleon@users.noreply.github.com>
2024-03-31 14:42:10 -0600

    More error catching

---
+++ 1.5/Source/VanillaBiomes/VanillaBiomesDefOf.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+
+namespace VanillaBiomes
+{
+    [DefOf]
+    public static class VanillaBiomesDefOf
+    {
+        public static BiomeDef ExtremeDesert;
+        public static BiomeDef ZBiome_AlpineMeadow;
+    }
+}
